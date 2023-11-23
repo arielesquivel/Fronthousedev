8b3081c67ed56e30e3f94375975dd87e4f47be1e
ariel <arielesquivelpcmail.com>
2023-11-23 13:57:37 -0300

    vista users

--- house-dev/src/componentes/VistaUsers.js
+++ house-dev/src/componentes/VistaUsers.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "../App";
 function VistaUsers() {
-  return <div>VistaUsers</div>;
+  return (
+    <>
+      <div className="container_3">
+        <div>
+          <h1>Mi prefil</h1>
+          <p>Nombre:</p>
+          <p>Apellido:</p>
+          <p>Contacto:</p>
+          <p>Email:</p>
+          <p>Contraseña:</p>
+          <div>
+            <img src="#" alt="#"></img>
+            <br />
+          </div>
+        </div>
+      </div>
+      <div className="btn-md">
+        <button>Cambiar</button>
+        <button>Editar informacion</button>
+      </div>
+    </>
+  );
 }
 
 export default VistaUsers;
