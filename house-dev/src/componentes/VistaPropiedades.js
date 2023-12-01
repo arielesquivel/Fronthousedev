ed000dec3ba417f46e6f57a670b6700eaa9125a5
ariel <arielesquivelpcmail.com>
2023-12-01 15:56:38 -0300

    carussel

--- house-dev/src/componentes/VistaPropiedades.js
+++ house-dev/src/componentes/VistaPropiedades.js
@@ -34,6 +34,49 @@ function VistaPropiedades() {
     return (
       <>
         <Navbar />
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="https://www.esneca.lat/wp-content/uploads/disen%CC%83o-interior.jpg"
+                alt="foto1"
+                class="d-block w-100"
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://novalproperties.com/images/projects/jardines-img-5.jpg"
+                alt="foto1"
+                class="d-block w-100"
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://www.esneca.lat/wp-content/uploads/disen%CC%83o-interior.jpg"
+                alt="foto1"
+                class="d-block w-100"
+              />
+            </div>
+          </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExample"
+            data-bs-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
         <div>
           <div className="img-vistadepropiedades">
             <img
